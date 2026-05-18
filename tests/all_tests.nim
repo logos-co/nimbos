3ac5a12c14fb0f5a03afccf1fb78e5828e51993c
aimendj <aimen.djari@gmail.com>
2026-05-18 19:44:48 +0100

    test: add Mantle tx decoding roundtrip tests

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -23,6 +23,7 @@ import # Unit test
   ./bedrock/mantle/test_proofs,
   ./bedrock/mantle/test_tx_types,
   ./bedrock/mantle/test_tx_encoding,
+  ./bedrock/mantle/test_tx_decoding,
   "./bedrock/block/test_block_types",
   "./bedrock/block/test_genesis",
   ./bedrock/sdp/test_state
